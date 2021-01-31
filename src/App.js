1205c7535816b80accc0767a9179c544842520a1
Sam Marston <sammarston15@gmail.com>
2021-01-30 17:22:48 -0700

    installed axios and successfully tested recipe api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const getRecipeTest = () => {
+    const options = {
+      method: 'GET',
+      url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/extract',
+      params: {url: 'http://www.melskitchencafe.com/the-best-fudgy-brownies/'},
+      headers: {
+        'x-rapidapi-key': '2701f858eemshfce0a3efecb24c0p12b082jsnb8953eaa8184',
+        'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+      }
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+
   return (
     <div className="App">
       hello world
+      <button onClick={getRecipeTest}>get recipe</button>
     </div>
   );
 }
